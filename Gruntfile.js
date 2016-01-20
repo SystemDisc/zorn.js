25589dd9edd0bc18e2af39c4539ad71cf13eeb48
Timothy Zorn <tim@zornco.com>
2016-01-20 18:20:30 -0500

    feat(Grunt): create Gruntfile for build process

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = function(grunt)
+{
+	var versionInfo = new require('./lib/versions/version-info')(grunt);
+
+	require('load-grunt-tasks')(grunt);
+
+	var Z_VERSION = versionInfo;
+	var dist = 'zorn-'+ Z_VERSION.full;
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		Z_VERSION: Z_VERSION,
+
+		jshint: {
+			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+			options: {
+				browserify: true,
+				browser: true,
+				devel: true,
+				globals: {
+					zorn: true
+				}
+			}
+		},
+
+		browserify: {
+			dist: {
+				files: {
+					'build/zorn.js': ['src/**/*.js']
+				}
+			}
+		},
+
+		clean: {
+			build: ['build/*']
+		}
+	});
+
+	grunt.registerTask('test', ['jshint']);
+
+	grunt.registerTask('build', ['browserify']);
+
+	grunt.registerTask('default', ['clean', 'test', 'build']);
+};
