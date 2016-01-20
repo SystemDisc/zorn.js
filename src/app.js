8296f8aa2e61a3629c060c8c2896a1512bd1a7f2
Timothy Zorn <tim@zornco.com>
2016-01-20 18:23:18 -0500

    feat(bootstrap): create simple bootstrap process

---
+++ src/app.js
@@ -0,0 +1,3 @@
+global.zorn = module.exports = require('./z');
+
+zorn.bootstrap();
---
+++ src/z/bootstrap.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var $ = require('jquery-browserify');
+
+module.exports = function()
+{
+	zorn.$rootElem = $('[z-app]');
+	if (zorn.$rootElem.length === 0)
+	{
+		zorn.$rootElem = $('body');
+	}
+	zorn.$rootElem = zorn.$rootElem[0];
+	zorn.$rootElem.$scope = {};
+};
